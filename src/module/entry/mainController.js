e00370c09ce915dd4c079245ca4d7f29c0d327f5
helinfeng <helinfeng@baijiahulian.com>
2016-05-06 10:18:53 +0800

    temp

--- src/module/entry/mainController.js
+++ src/module/entry/mainController.js
@@ -1,6 +1,74 @@
 define(function (require) {
     function mainController($scope, $location, $interval, $window, $timeout, $rootScope) {
+    	$scope.isLoginPage = function () {
+    		if ($location.$$path == '/login') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isLoginPage = $scope.isLoginPage;
 
+    	$scope.isProfilePage = function () {
+    		if ($location.$$path == '/profile') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isProfilePage = $scope.isProfilePage;
+
+    	$scope.isGeographyPage = function () {
+    		if ($location.$$path == '/geography') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isGeographyPage = $scope.isGeographyPage;
+
+    	$scope.isManipulationPage = function () {
+    		if ($location.$$path == '/manipulation') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isManipulationPage = $scope.isManipulationPage;
+
+    	$scope.isStatisticPage = function () {
+    		if ($location.$$path == '/statistic') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isStatisticPage = $scope.isStatisticPage;
+
+    	$scope.isFaultPage = function () {
+    		if ($location.$$path == '/fault') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isFaultPage = $scope.isFaultPage;
+
+    	$scope.isSetPage = function () {
+    		if ($location.$$path == '/set') {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	};
+    	$rootScope.isSetPage = $scope.isSetPage;
     }
 
 
